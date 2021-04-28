d151b1e395357f5ec1ea22c78b75d38f13aeec89
earthboy13 <waterboy923@hotmail.com>
2021-04-28 07:00:34 -0400

    minor fix

--- lib/services/networking.dart
+++ lib/services/networking.dart
@@ -6,8 +6,6 @@ class NetworkMangaer {
   Uri uri;
 
   Future getData({String url}) async {
-    //String apiKey = 'ece777c5b3c9b0e99d4c130e66b0664f';
-    //'https://api.openweathermap.org/data/2.5/weather?lat=$lat&lon=$log&appid=$apiKey'
     uri = Uri.parse(url);
     print(uri);
     http.Response response = await http.get(uri);
